023d91773e1b78de46bc393fd2089e4e8fa84556
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-02-22 12:29:39 -0500

    resolver query partially done

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -45,3 +45,57 @@ const validateSalary = (salary) => {
 }
 
 
+const resolvers = {
+    Date: DateScalar,
+    
+    Query: {
+        login: async (_, {loginInput}) => {
+            const {username, password} = loginInput;
+
+            if (!username || !password) {
+                throw new UserInputError('Username and password are required');
+            }
+            const user = await User.findByCredentials(username);
+            if (!user) {
+                throw new AuthenticationError('Invalid username ');
+            }
+
+            const isPasswordValid = await user.isPasswordValid(password);
+            if (!isPasswordValid) {
+                throw new AuthenticationError('Invalid password');
+            }
+            return{
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                created_at: user.created_at,
+                updated_at: user.updated_at
+            };
+        },
+
+        getAllEmployees: async () => {
+            const employees = (await Employee.find()).sort({created_at: -1});
+            return employees.map(employee => ({
+                id: employee._id,
+                first_name: employee.first_name,
+                last_name: employee.last_name,
+                email: employee.email,
+                created_at: employee.created_at,
+                updated_at: employee.updated_at
+            }));
+        },
+        getEmployeeById: async (_, {id}) => {
+            const employee = await Employee.findById(id);
+            if (!employee) {
+                throw new UserInputError('Employee not found');
+            }
+            return {id: employee._id, ...employee.toObject()};
+        },
+
+        searchEmployeeByDesginationOrDepartment: async (_, {designation, department}) => {
+            if (!designation && !department) {
+                throw new UserInputError('At least one of designation or department must be provided');
+            }
+            
+    }
+};
