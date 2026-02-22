fac89293b6e1f8748c291f5cfc321a13dfca7a18
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-02-22 11:10:51 -0500

    Employee model partial

---
+++ models/Employee.js
@@ -0,0 +1,69 @@
+const e = require('express');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const employeeSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: [true, 'First name is required'],
+        trim: true,
+        minlength: [2, 'First name must be at least 2 characters long'],
+        maxlength: [50, 'First name must be less than 50 characters long']
+    },
+    last_name: {
+        type: String,
+        required: [true, 'Last name is required'],
+        trim: true,
+        minlength: [2, 'Last name must be at least 2 characters long'],
+        maxlength: [50, 'Last name must be less than 50 characters long']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        validate: {
+            validator: function(email) {
+                return validator.isEmail(email);
+            },
+            message: 'Please enter a valid email address'
+        }
+    },
+    gender:{
+        type: String,
+    },
+    designation: {
+        type: String,
+        required: [true, 'Designation is required'],
+        trim: true
+    },
+    salary:{
+        type: Number,
+        required: [true, 'Salary is required'],
+        min: [1000, 'Salary must be at least 1000']
+    },
+    date_of_joining: {
+        type: Date,
+        required: [true, 'Date of joining is required']
+    },
+    department: {
+        type: String,
+        required: [true, 'Department is required'],
+        trim: true
+    },
+    employee_photo:{
+        type: String,
+        default: null
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at:{
+        type: Date,
+        default: Date.now
+    }
+}, 
+{   
+    timestamps: false,
+    versionKey: false
+});
