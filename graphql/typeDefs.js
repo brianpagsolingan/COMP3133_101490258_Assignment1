a8ecdbad43f504d623667b5ddc62f9fbc7319582
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-02-22 15:38:26 -0500

    finished typeDefs and added a hook to update the updated_at filed

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -2,7 +2,7 @@ const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
     scalar Date
-    
+    # User type definition
     type User{
         id: ID!
         username: String!
@@ -10,7 +10,7 @@ const typeDefs = gql`
         created_at: Date!
         updated_at: Date!
         }
-        
+    # Employee type definition    
     type Employee{
         id: ID!
         first_name: String!
@@ -26,6 +26,78 @@ const typeDefs = gql`
         updated_at: Date
         }
 
-    `;
+    
+    type EmployeeResponse{
+        success: Boolean!
+        message: String
+        employee: Employee
+    }
+
+    type DeleteResponse{
+        success: Boolean!
+        message: String
+    }
+    
+    #Input types 
+    
+    input SignupInput{
+        username: String!
+        email: String!
+        password: String!
+    }
+    
+    input LoginInput{
+        username: String!
+        password: String!
+    }   
+    
+    input AddEmployeeInput{
+        first_name: String!
+        last_name: String!
+        email: String!
+        gender: String
+        designation: String!
+        salary: Float!
+        date_of_joining: Date!
+        department: String!
+        employee_photo: String
+    }
+    
+    input UpdateEmployeeInput{
+        first_name: String
+        last_name: String
+        email: String
+        gender: String
+        designation: String
+        salary: Float
+        date_of_joining: Date
+        department: String
+        employee_photo: String
+    }
+        
+    # Queries
+
+    type Query{
+        login(loginInput: LoginInput!): User!
+
+        getAllEmployees: [Employee!]!
+        getEmployeeById(id: ID!): Employee
+
+        searchEmployeesByDepartment(
+        department: String!
+        designation: String!
+        ): [Employee!]!
+
+    }
+    
+    # Mutations
+        
+    type Mutation{
+        signup(signupInput: SignupInput!): User!
+        addEmployee(addEmployeeInput: AddEmployeeInput!): EmployeeResponse!
+        updateEmployee(id: ID!, updateEmployeeInput: UpdateEmployeeInput!): EmployeeResponse!
+        deleteEmployee(id: ID!): DeleteResponse!
+    }
+`;
 
 module.exports = typeDefs;
