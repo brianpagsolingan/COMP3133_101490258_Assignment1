b3e2e560ee2c6369339a9deab3ed42ba9291f5d8
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-02-22 15:43:13 -0500

    updated user model

--- models/User.js
+++ models/User.js
@@ -41,6 +41,35 @@ const userSchema = new mongoose.Schema({
     versionKey: false
 });
 
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(12);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+//update the updated_at field on update
+userSchema.pre('findOneAndUpdate', function(next) {
+    this.set({ updated_at: new Date() });
+    next();
+});
+
+//method to compare password for login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+//find by email or username
+userSchema.statics.findByCredentials = async function (identifier){
+    const user = await this.findOne({ $or: [{ email: identifier }, { username: identifier }] });
+    return user;
+}
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
